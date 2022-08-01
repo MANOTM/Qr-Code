e216e5759ddaf93f53101bc6e34183088e0cf2a5
OTMANE MANSOURI <96500601+MANOTM@users.noreply.github.com>
2022-08-02 00:00:30 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,15 @@ const btn = document.getElementById("btn"),
   container = document.querySelector(".container"),
   qrDiv = document.querySelector(".qr"),
   qrImg = document.querySelector(".qr img"),
-  text = document.getElementById("text");
+  text = document.getElementById("text"),
+  Donwlaod = document.getElementById("Donwlaod"),
+  fileInpu = document.getElementById("file"),
+  selectImg = document.querySelector(".cloud"),
+  upload = document.querySelector(".upload"),
+  p = selectImg.querySelector("p"),
+  textarea = document.querySelector("textarea"),
+  copyBtn = document.getElementById("copy"),
+  closeBtn = document.getElementById("close");
 
 btn.addEventListener("click", (eo) => {
   let value = text.value.trim();
@@ -28,7 +36,53 @@ function getImg(value) {
   qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${value}`;
   qrImg.addEventListener("load", () => {
     btn.innerText = "Generatar Qr Code";
-    container.style.height = "500px";
+    container.style.height = "550px";
     qrDiv.style.opacity = "1";
   });
 }
+
+Donwlaod.onclick = (eo) => {
+  eo.preventDefault();
+  Donwlaod.href = qrImg.src;
+};
+
+selectImg.onclick = () => {
+  fileInpu.click();
+};
+fileInpu.addEventListener("change", (eo) => {
+  let file = eo.target.files[0];
+  if (!file) return;
+  form = new FormData();
+  form.append("file", file);
+  getData(form, file);
+});
+function getData(form, file) {
+  p.innerHTML = "Scanning QR Code...";
+  fetch("http://api.qrserver.com/v1/read-qr-code/", {
+    method: "POST",
+    body: form,
+  })
+    .then((res) => res.json())
+    .then((resul) => {
+      resul = resul[0].symbol[0].data;
+      p.innerHTML = resul
+        ? "Upload QR Code to Scan"
+        : "Sorry We Couldn't Scan ";
+      if (!resul) return;
+      textarea.innerHTML = resul;
+      selectImg.querySelector("img").src = URL.createObjectURL(file);
+      upload.classList.add("show");
+      selectImg.querySelector(".imgg").classList.add("activ");
+    })
+    .catch(() => {
+      p.innerHTML = "Sorry We Couldn't Scan ";
+    });
+}
+copyBtn.onclick = () => {
+  let value = textarea.textContent;
+  navigator.clipboard.writeText(value);
+};
+closeBtn.onclick = () => {
+  upload.classList.remove("show");
+  selectImg.querySelector(".imgg").classList.remove("activ");
+};
